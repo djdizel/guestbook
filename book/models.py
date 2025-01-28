5badf6e9c144f3883e54bb8edb27e6d6d4aa38a4
djdizel <158520404+djdizel@users.noreply.github.com>
2025-01-28 22:16:38 +0200

    123

--- book/models.py
+++ book/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class GuestbookEntry(models.Model):
-    name = models.CharField(max_length=100) # Имя Гостя
+    name = models.CharField(max_length=100) 
     message = models.TextField() # Сообщение
-    created_at = models.DateTimeField(auto_now_add=True) #Автомат отметка времени
+    created_at = models.DateTimeField(auto_now_add=True) 
 def __str__(self):
     return f"{self.name} ({self.created_at.strftime('%Y-%m-%d %H:%M')})"
